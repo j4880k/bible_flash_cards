76745376688223197e55b01d04dfd041d1731270
rwheadon <rich.wheadon@gmail.com>
2020-10-25 17:02:43 -0400

    updated for 2021 with Davey

--- app/app.js
+++ app/app.js
@@ -10,8 +10,8 @@
 			this.available_books = [];
 			this.available_questions = [];
 			// this.available_decks = [{"code":"LTLL-2017-BB","label":"LTLL 2017"}];
-			this.available_decks = [{"code":"LTLL-2020-BB","label":"LTLL 2020"}];
-			this.selected_deck = "LTLL-2018-BB";
+			this.available_decks = [{"code":"LTLL-2021-BB","label":"LTLL 2021"}];
+			this.selected_deck = "LTLL-2021-BB";
 			this.chosen_book_chapters = [];
 			this.chosen_set = "";
 			this.active_card = 0;
--- app/data/available_decks.js
+++ app/data/available_decks.js
@@ -1,7 +1,7 @@
 var available_decks = [
 	{
 		"id":"01",
-		"code":"LTLL-2020-BB",
-		"label":"2020 LTLL Bible Bowl"
+		"code":"LTLL-2021-BB",
+		"label":"2021 LTLL Bible Bowl"
 	}
 ]
